6b8f465541eb423b64b80c6b67091f841b905c47
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 16:50:09 +0530

    added Ar5

---
+++ Ar5.java
@@ -0,0 +1,12 @@
+class Ar5{
+	public static void main (String []a){
+		float[]ear ={45.040f,6.567f,55.5655f,67,54.54f,334.44f,33.45f,-678.55f,-998.89f};
+		System.out.println(ear[0]);
+		System.out.println(ear[4]);
+		System.out.println(ear[2]);
+		System.out.println(ear[3]);
+		System.out.println(ear[1]);
+		System.out.println(ear[7]);
+		System.out.println(ear[8]);
+	}
+}
